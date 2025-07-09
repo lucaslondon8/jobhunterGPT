c20e0009f0971efd6d1199f913a61ccac64cdaf6
Lux <lux@Plois-MacBook-Pro.local>
2025-07-09 22:13:16 +0100

    Updated with Cover Letter Generation and In-App email send function

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="jobhunterGPT",
+    name="jobhunt-gpt",
     version="0.1.0",
     packages=find_packages(),
+    python_requires=">=3.8",
 )
